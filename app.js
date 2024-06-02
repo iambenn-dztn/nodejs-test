3d25930727110360ee1cf0303fdd3599d3b9e6cb
DiemNT 20 <diemnt20@fpt.com>
2024-06-02 15:33:28 +0700

    [Task 3] add middle ware to secure app

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-import express from "express";
 import bodyParser from "body-parser";
+import express from "express";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 import db from "./src/configs/sequelize.js";
-import router from "./src/routes/index.js";
 import { errorHandler } from "./src/middlewares/error.middleware.js";
+import router from "./src/routes/index.js";
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(limiter);
 
 // Routes
 app.use("/api", router);
